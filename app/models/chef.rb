43178d16ed732d7a9f7864192b173e2a8da38702
Mario Rico <mariorico@gmail.com>
2015-03-28 16:56:57 +0000

    chef test and validations

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,9 @@
 class Chef < ActiveRecord::Base
-  #Validation
+  # Validation
+  validates :name, presence: true, length:{minimum:3,maximum:4}
+  validates :email, presence: true, email: true,uniqueness: true
   
+  # Relationships
   has_many :recipes
   
 end
