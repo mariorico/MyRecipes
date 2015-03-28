f9e7cfdabb197ffbcf1befca5d139ab9c446b33e
Mario Rico <mariorico@gmail.com>
2015-03-28 18:22:29 +0000

    chef validations and test updated

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,10 +1,12 @@
 class Chef < ActiveRecord::Base
   # Validation
   validates :name, presence: true, length:{minimum:3,maximum:4}
-  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true,uniqueness: {case_sensitive: false}, format:{with: VALID_EMAIL_REGEX}
   
   # Relationships
   has_many :recipes
   
+  # Callbacks
+  before_save{self.email = email.downcase}
 end
